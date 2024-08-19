a2166d6ce23e9b26484fec1ef551c7b7f9df4493
ogkenni <147646943+ogkenni@users.noreply.github.com>
2024-08-19 16:03:36 +0100

    Update Checkout.jsx

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,9 +1,6 @@
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import './Checkout.module.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useNavigate, useLocation } from 'react-router-dom';
 
 const Checkout = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -12,47 +9,8 @@ const Checkout = () => {
     const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
  const userId = queryParams.get('userId');
-  const token = localStorage.getItem('token'); // Assuming you store the token in sessionStorage
+  const token = localStorage.getItem('token'); 
 
-  useEffect(() => {
-     const userId = queryParams.get('userId');
-  const token = localStorage.getItem('token');
-    const fetchCartItems = async () => {
-      try {
-        const response = await axios.get(
-          `https://silver-gray-stem.glitch.me/api/cart/${userId}`,
-          {
-            headers: {
-              Authorization: token,
-            },
-          }
-        );
-        setCartItems(response.data);
-      } catch (error) {
-        console.error('There was an error fetching the cart items!', error);
-        setError('Failed to load cart items. Please try again later.');
-      }
-    };
-
-    fetchCartItems();
-  }, [userId, token]);
-
-
-
-  if (error) {
-    return (
-      <>
-        <Header />
-        <div className="container my-5 mx-5">
-          <h1 className="container my-5 py-5">{error}</h1>
-          <button onClick={() => navigate('/dashboard')}>
-            Go Back to Shop
-          </button>
-        </div>
-        <Footer />
-      </>
-    );
-  }
 
   if (cartItems.length === 0) {
     return (
