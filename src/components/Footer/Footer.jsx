754ce1c22d08396f60e2e0a3f4f08561dce786f6
ogkenni <147646943+ogkenni@users.noreply.github.com>
2024-08-21 08:38:40 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -76,6 +76,7 @@ const Footer = () => {
 
         <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
           <p>© 2024 Company, Inc. All rights reserved.</p>
+          <p>Web app developed by Ogkenni, CEO of WebOnChain Services.</p>
           <ul className="list-unstyled d-flex">
             <li className="ms-3">
               <a className="link-body-emphasis" href="#">
